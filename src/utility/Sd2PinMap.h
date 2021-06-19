12a0fbd99bbdc3711a8f96128011bc8a5917970b
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2021-06-19 18:59:57 -0400

    Update Sd2PinMap.h

--- src/utility/Sd2PinMap.h
+++ src/utility/Sd2PinMap.h
@@ -31,7 +31,7 @@
 
 #endif // Sd2PinMap_h
 
-#elif defined(__AVR_ATmega4809__) // Arduino UNO WiFI Rev2 follows
+#elif defined(__AVR_ARCH__) && defined(__AVR_ARCH__) > 100 ) // Arduino UNO WiFI Rev2 follows
 
 #ifndef Sd2PinMap_h
   #define Sd2PinMap_h
